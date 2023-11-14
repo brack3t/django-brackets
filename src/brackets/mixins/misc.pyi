a3480261ecd715f2fbffcfd1c72bcd8fccf0d5ee
Kenneth Love <klove@oreilly.com>
2023-11-14 19:51:01 +0000

    StaticContextMixin is unnecessary

--- src/brackets/mixins/misc.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Any, ClassVar
-
-from . import HasContext
-
-class StaticContextMixin(HasContext):
-    context: ClassVar[dict[str, Any]]
-    static_context: dict[str, Any]
-    def get_static_context(self) -> dict[str, Any]: ...
-    def get_context_data(self, **kwargs: dict[Any, Any]) -> dict[str, Any]: ...
